3087d45371ace25cad85657b7a60071ad610f8bb
bansaih11 <bansaihbakir86@gmail.com>
2023-08-05 21:17:23 +0300

    /

--- app/src/main/java/com/ba/first_application/MainActivity.kt
+++ app/src/main/java/com/ba/first_application/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ba.first_application
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.first_application.R
 import com.example.first_application.databinding.ActivityMainBinding
     class MainActivity: AppCompatActivity() {
 
@@ -12,38 +15,64 @@ import com.example.first_application.databinding.ActivityMainBinding
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
 
-            binding.loginButton.setOnClickListener {
-                val username=binding.usernameEditText.text
-                val password=binding.passwordEditText.text
-                var sports=""
-                var gender=""
-                if(binding.footballCk.isChecked){
-                    sports+="Football,"
-                }
-                if(binding.swimmingCk.isChecked){
-                    sports+="Swimming,"
-                }
-                if(binding.handballCk.isChecked){
-                    sports+="Handball "
-                }
-                if(binding.maleRB.isChecked){
-                    gender="Male"
-                }else{
-                    gender="Female"
-                }
-
-             if ((username.isNotEmpty())&&(password.isNotEmpty())&&(binding.footballCk.isChecked || binding.swimmingCk.isChecked||binding.handballCk.isChecked)&&(binding.maleRB.isChecked || binding.femaleRB.isChecked)){
-              Toast.makeText(this,"Hi $username  your hobbies : $sports  Gender is: $gender"   ,Toast.LENGTH_LONG ).show()
-           }
-                else{
+           binding.loginButton.setOnClickListener {
+               val username=binding.usernameEditText.text
+               val password=binding.passwordEditText.text
+               var gender=""
+               var sports=""
+               if(binding.footballCk.isChecked){
+                   sports+="Football,"
+               }
+               if(binding.swimmingCk.isChecked){
+                   sports+="Swimming,"
+               }
+               if(binding.handballCk.isChecked){
+                   sports+="Handball "
+               }
+               if(binding.maleRB.isChecked){
+                   gender="Male"
+               }else{
+                   gender="Female"
+               }
+             /*  if ((username.isNotEmpty())&&(password.isNotEmpty())&&
+                   ((binding.footballCk.isChecked)|| (binding.swimmingCk.isChecked) ||(binding.handballCk.isChecked))
+                   &&((binding.maleRB.isChecked)|| (binding.femaleRB.isChecked))){
+                   Toast.makeText(this,"Hi $username  your hobbies : $sports  Gender is: $gender"   ,Toast.LENGTH_SHORT ).show()
+               }
+               else{
                    Toast.makeText(this,"Please go back ",Toast.LENGTH_LONG ).show()
-                }
+               }*/
+
+                val intent=Intent(this,SecondActivity::class.java)
+                intent.putExtra("USERNAME",binding.usernameEditText.text.toString())
+               intent.putExtra("message","Hi $username  your hobbies : $sports  Gender is: $gender")
+                startActivityForResult(intent,11)
+
+
             }
             binding.backbutton.setOnClickListener {
 
-                Toast.makeText(this,"Thank you ! ",Toast.LENGTH_LONG ).show()
+               Toast.makeText(this,"Thank you ! ",Toast.LENGTH_LONG ).show()
+
+                }
+            }
+
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if(requestCode==11){
+                if (resultCode==1){
+                    Toast.makeText(this,"Logged in by Google",Toast.LENGTH_LONG).show()
+                }
+                else if(resultCode==2){
+                    Toast.makeText(this,"Logged in by Facebook",Toast.LENGTH_LONG).show()
+                }
 
             }
         }
-    }
+
+        }
+
+
+
 
---
+++ app/src/main/java/com/ba/first_application/SecondActivity.kt
@@ -0,0 +1,37 @@
+package com.ba.first_application
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.first_application.databinding.ActivitySecondBinding
+
+lateinit var binding: ActivitySecondBinding
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var username=intent.extras?.getString("USERNAME","")
+        binding.usernameSecond.text= username
+
+        Toast.makeText(this,intent.extras?.getString("message"),Toast.LENGTH_LONG).show()
+
+
+       binding.okButton.setOnClickListener {
+           val resultCode= if (binding.googleRB.isChecked){
+               1
+           }else {
+               2
+           }
+           val intent=Intent()
+           setResult(resultCode,intent)
+           finish()
+       }
+    }
+
+
+
+
+}
